d348c62d052840f983ae0aeed50ef8d9d4fcead5
Rohama Salfi <rohxama@gmail.com>
2025-03-24 10:11:49 +0500

    Making animation of text reaveling

--- script/script.js
+++ script/script.js
@@ -10,3 +10,17 @@ document.addEventListener('click', e => {
         cursor.classList.remove("expand");
     }, 500);
 });
+
+// JavaScript code to change text on hover and apply gradient background
+const spanText = document.querySelector('.text');
+
+document.querySelector('.hover-effect').addEventListener('mouseover', function() {
+  spanText.textContent = "real-world problems"; // Change text on hover
+  spanText.classList.add('text-hover'); // Apply gradient effect
+});
+
+document.querySelector('.hover-effect').addEventListener('mouseout', function() {
+  spanText.textContent = "creative solutions"; // Revert text when hover ends
+  spanText.classList.remove('text-hover'); // Remove gradient effect
+});
+
